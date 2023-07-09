e6f77e7a6cd05c0831262ce8bd32263bb4b03667
Dorcas <dorcasingasha@gmail.com>
2023-07-10 01:11:28 +0300

    Connect to Database and test connection

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const http = require("http");
+require("./config/dbConnect");
 const app = require("./app/app");
 
 const PORT = process.env.PORT || 2020;
 
- //server
+//server
 const server = http.createServer(app);
 server.listen(PORT, console.log(`Server is running on port ${PORT}`));
